5b292cafcf048cfc5ef41b4650edf5d3117318ee
ian-ramos <ramos.ianc@gmail.com>
2017-12-12 16:59:19 -0500

    another change to cors origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'set-game-backend.herokuapp.com'
+    origins 'https://set-game-backend.herokuapp.com'
 
     resource '*',
       headers: :any,
